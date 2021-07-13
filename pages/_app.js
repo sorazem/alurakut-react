d3dbd43097e51ba61fdf3ce1eb1eca848eb91135
Stephanie <stephanieorazem@jovensgenios.com>
2021-07-12 22:04:01 -0300

    :tada: Aula 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,50 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { AlurakutStyles } from '../src/lib/AlurakutCommons';
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
-`
+
+  body {
+    background-color: #fbe8e2;
+    font-family: sans-serif;
+  }
+
+  #__next{
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  img{
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+
+  ${AlurakutStyles}
+`;
 
 const theme = {
   colors: {
-    primary: '#0070f3',
-  },
-}
+    pinkpeach: "#f98b88",
+    backgroundDefault: "#D9E6F6",
+    backgroundSecondary: "#F1F9FE",
+    primaryText: "#2E7BB4",
+    secondaryText: "#388BB0",
+    tertiaryText: "#2F4A71",
+    primaryElement: "#6F92BB",
+    anotherElement: "#5579A1",
+    gray1: "#333333",
+    gray2: "#5A5A5A",
+    gray3: "#999999",
+    gray4: "#C5C6CA",
+    gray5: "#F4F4F4"
+  }
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +54,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,89 @@
-import styled from 'styled-components'
+import MainGrid from "../src/components/MainGrid";
+import Box from "../src/components/Box";
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
+import followers from "../src/content/followers.js"
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSidebar(prop) {
+  return (
+    <Box>
+      <img src={`https://github.com/${prop.githubUser}.png`} style={{ borderRadius: '8px' }} />
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = "sorazem";
+  const name = "Stephanie"
+  const communityPeople = [
+    'juunegreiros',
+    'omariosouto',
+    'peas',
+    'rafaballerini',
+    'marcobrunodev',
+    'felipefialho'
+  ]
+
+  return (
+    <>
+    <AlurakutMenu githubUser={githubUser}/>
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar githubUser={githubUser} />
+        </div>
+
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className="title">
+              Bem vindo(a), {name}
+            </h1>
+
+            <OrkutNostalgicIconSet recados={25} fotos={5} videos={2} fas={7} confiavel={3} legal={2} sexy={1}/>
+          </Box>
+        </div>
+
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidade ({communityPeople.length})
+            </h2>
+
+            <ul>
+              {communityPeople.map((itemAtual) => {
+                return (
+                  <li>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                      <img src={`https://github.com/${itemAtual}.png`} />
+                      <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Seguidores ({followers.length})
+            </h2>
+
+            <ul style={{marginBottom: "16px"}}>
+              {followers.map((follower) => {
+                return (
+                  <li>
+                    <a href={`/users/${follower.login}`} key={follower.login}>
+                      <img src={`https://github.com/${follower.login}.png`} />
+                      <span>{follower.login}</span>
+                    </a>
+                  </li>
+                )
+              }).splice(0, 6)}
+            </ul>
+
+            <a style={{fontWeight: "bold", fontSize: "14px", marginTop: "16px", color: "#b05c59"}}>Ver todos</a>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  )
 }
